b736554c890c20ec471f639315c9b181c24c69c5
Ryan Dainton <ryandainton@live.com>
2024-04-24 15:41:38 +0100

    diffview open/close keymappings

--- lua/custom/plugins/diffview.lua
+++ lua/custom/plugins/diffview.lua
@@ -1,5 +1,12 @@
 return {
   {
     'sindrets/diffview.nvim',
+
+    config = function()
+      local diffview = require 'diffview'
+
+      vim.keymap.set('n', '<leader>go', diffview.open, { desc = 'Diffview [G]it [O]pen' })
+      vim.keymap.set('n', '<leader>gc', diffview.close, { desc = 'Diffview [G]it [C]lose' })
+    end,
   },
 }
