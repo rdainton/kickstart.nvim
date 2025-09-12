bd446c6711e1717be943d484f864bfd3acbfa3ed
Ryan Dainton <ryandainton@live.com>
2025-09-12 06:20:16 +0100

    fix: remove redundant plugin

--- init.lua
+++ init.lua
@@ -183,35 +183,7 @@ require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
 
-  -- NOTE: Plugins can also be added by using a table,
-  -- with the first argument being the link and the following
-  -- keys can be used to configure plugin behavior/loading/etc.
-  --
-  -- Use `opts = {}` to force a plugin to be loaded.
-  --
-  --  This is equivalent to:
-  --    require('Comment').setup({})
-
-  -- "gc" to comment visual regions/lines
-  { 'numToStr/Comment.nvim', opts = {} },
-
-  -- Here is a more advanced example where we pass configuration
-  -- options to `gitsigns.nvim`. This is equivalent to the following Lua:
-  --    require('gitsigns').setup({ ... })
-  --
-  -- See `:help gitsigns` to understand what the configuration keys do
-  { -- Adds git related signs to the gutter, as well as utilities for managing changes
-    'lewis6991/gitsigns.nvim',
-    opts = {
-      -- signs = {
-      -- add = { text = '+' },
-      -- change = { text = '~' },
-      -- delete = { text = '_' },
-      -- topdelete = { text = '‾' },
-      -- changedelete = { text = '~' },
-      -- },
-    },
-  },
+  { 'lewis6991/gitsigns.nvim', opts = {} },
 
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
   --
